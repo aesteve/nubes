edc39722f4534b6060a94b9b1c4905ed8c81d2ce
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-06-28 14:56:37 +0200

    onSuccessOnly, onFutureOnly

--- src/main/java/com/github/aesteve/vertx/nubes/utils/async/AsyncUtils.java
+++ src/main/java/com/github/aesteve/vertx/nubes/utils/async/AsyncUtils.java
@@ -3,8 +3,12 @@ package com.github.aesteve.vertx.nubes.utils.async;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 public class AsyncUtils {
+	
+	private static final Logger log = LoggerFactory.getLogger(AsyncUtils.class);
 	
 	public static<T> Handler<AsyncResult<T>> completeOrFail(Future<T> fut) {
 		return (res -> {
@@ -31,4 +35,23 @@ public class AsyncUtils {
 			}
 		});
 	}
+	
+	public static<T> Handler<AsyncResult<T>> onSuccessOnly(Handler<T> handler) {
+		return (res -> {
+			if (res.failed()) {
+				log.warn("Exception has been swallowed by AsyncUtils.onSuccess", res.cause());
+				return;
+			}
+			handler.handle(res.result());
+		});
+	}
+
+	public static<T> Handler<AsyncResult<T>> onFailureOnly(Handler<T> handler) {
+		return (res -> {
+			if (res.failed()) {
+				handler.handle(res.result());
+			}
+		});
+	}
+
 }
