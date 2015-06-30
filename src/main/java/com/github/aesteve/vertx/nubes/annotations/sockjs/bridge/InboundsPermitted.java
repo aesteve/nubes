2aa2c763e4fc80cb3e70865573ad5f736492f94b
Louis-Marie Dallen <lm.dallen@treeptik.fr>
2015-06-30 13:32:31 +0200

    added Repeatable annotations

---
+++ src/main/java/com/github/aesteve/vertx/nubes/annotations/sockjs/bridge/InboundsPermitted.java
@@ -0,0 +1,16 @@
+package com.github.aesteve.vertx.nubes.annotations.sockjs.bridge;
+
+import com.github.aesteve.vertx.nubes.annotations.sockjs.bridge.InboundPermitted;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface InboundsPermitted {
+
+	InboundPermitted[] value();
+
+}
---
+++ src/main/java/com/github/aesteve/vertx/nubes/annotations/sockjs/bridge/OutboundsPermitted.java
@@ -0,0 +1,14 @@
+package com.github.aesteve.vertx.nubes.annotations.sockjs.bridge;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface OutboundsPermitted {
+
+	OutboundPermitted[] value();
+
+}
