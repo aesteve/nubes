619d341fc60422e8f21fb0916ed8ab27b0ccc6b3
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-10-31 12:20:30 +0100

    missing files

---
+++ src/main/java/com/github/aesteve/vertx/nubes/exceptions/params/InvalidParamValueException.java
@@ -0,0 +1,21 @@
+package com.github.aesteve.vertx.nubes.exceptions.params;
+
+public class InvalidParamValueException extends WrongParameterException {
+
+	private static final long serialVersionUID = -8469141240265252774L;
+
+	private Object paramValue;
+	
+	public InvalidParamValueException(ParamType type, String paramName, Object paramValue) {
+		super(type, paramName);
+		this.paramValue = paramValue;
+	}
+	
+	public String getMessage() {
+		if (type == null && paramName == null && paramValue == null) {
+			return "Some request of form parameter has an invalid value";
+		}
+		return "Invalid value : " + paramValue + " for " + type.toString() + " : " + paramName;
+	}
+	
+}
