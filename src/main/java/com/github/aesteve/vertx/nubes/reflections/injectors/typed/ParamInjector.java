2ad8c93d2a2e1bf85c183efc49c1de3caf371227
aesteve <aesteve@evenium.com>
2015-11-23 15:36:10 +0100

    param injector as functional interface

--- src/main/java/com/github/aesteve/vertx/nubes/reflections/injectors/typed/ParamInjector.java
+++ src/main/java/com/github/aesteve/vertx/nubes/reflections/injectors/typed/ParamInjector.java
@@ -2,6 +2,7 @@ package com.github.aesteve.vertx.nubes.reflections.injectors.typed;
 
 import io.vertx.ext.web.RoutingContext;
 
+@FunctionalInterface
 public interface ParamInjector<T> {
 	public T resolve(RoutingContext context);
 }
