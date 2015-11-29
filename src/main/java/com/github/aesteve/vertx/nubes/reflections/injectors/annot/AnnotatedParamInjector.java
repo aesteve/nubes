c0ed796655cb3f7724c5a3a1093b09eafe84c0ae
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-11-29 16:12:10 +0100

    make it a functional interface

--- src/main/java/com/github/aesteve/vertx/nubes/reflections/injectors/annot/AnnotatedParamInjector.java
+++ src/main/java/com/github/aesteve/vertx/nubes/reflections/injectors/annot/AnnotatedParamInjector.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Annotation;
 
 import com.github.aesteve.vertx.nubes.exceptions.params.WrongParameterException;
 
+@FunctionalInterface
 public interface AnnotatedParamInjector<T extends Annotation> {
 	public Object resolve(RoutingContext context, T annotation, String paramName, Class<?> resultClass) throws WrongParameterException;
 }
