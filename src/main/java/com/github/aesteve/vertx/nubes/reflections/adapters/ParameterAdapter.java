cbdbf44701104da48b8798f1ff51c2181d0dcf0f
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-04-23 17:41:19 +0200

    crlf

--- src/main/java/com/github/aesteve/vertx/nubes/reflections/adapters/ParameterAdapter.java
+++ src/main/java/com/github/aesteve/vertx/nubes/reflections/adapters/ParameterAdapter.java
@@ -3,7 +3,9 @@ package com.github.aesteve.vertx.nubes.reflections.adapters;
 import io.vertx.core.MultiMap;
 
 public interface ParameterAdapter<T> {
+
 	T adaptParam(String value, Class<? extends T> parameterClass);
 
 	T adaptParams(MultiMap map, Class<? extends T> parameterClass);
+
 }
