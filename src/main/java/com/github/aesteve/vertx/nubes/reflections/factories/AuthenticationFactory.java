33805f978c0b999b126d28eb697b0df3d370fcc6
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-04-23 18:04:33 +0200

    reduce complexity

--- src/main/java/com/github/aesteve/vertx/nubes/reflections/factories/AuthenticationFactory.java
+++ src/main/java/com/github/aesteve/vertx/nubes/reflections/factories/AuthenticationFactory.java
@@ -1,5 +1,9 @@
 package com.github.aesteve.vertx.nubes.reflections.factories;
 
+import com.github.aesteve.vertx.nubes.Config;
+import com.github.aesteve.vertx.nubes.annotations.auth.Auth;
+import com.github.aesteve.vertx.nubes.auth.AuthMethod;
+import com.github.aesteve.vertx.nubes.handlers.impl.CheckTokenHandler;
 import io.vertx.core.Handler;
 import io.vertx.ext.auth.AuthProvider;
 import io.vertx.ext.web.RoutingContext;
@@ -7,17 +11,23 @@ import io.vertx.ext.web.handler.BasicAuthHandler;
 import io.vertx.ext.web.handler.JWTAuthHandler;
 import io.vertx.ext.web.handler.RedirectAuthHandler;
 
-import com.github.aesteve.vertx.nubes.Config;
-import com.github.aesteve.vertx.nubes.annotations.auth.Auth;
-import com.github.aesteve.vertx.nubes.auth.AuthMethod;
-import com.github.aesteve.vertx.nubes.handlers.impl.CheckTokenHandler;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import static com.github.aesteve.vertx.nubes.auth.AuthMethod.*;
 
 public class AuthenticationFactory {
 
 	private final Config config;
+	private Map<AuthMethod, Function<AuthProvider, Handler<RoutingContext>>> authHandlers;
 
 	public AuthenticationFactory(Config config) {
 		this.config = config;
+		authHandlers = new HashMap<>();
+		authHandlers.put(BASIC, BasicAuthHandler::create);
+		authHandlers.put(JWT, JWTAuthHandler::create);
+		authHandlers.put(API_TOKEN, CheckTokenHandler::new);
 	}
 
 	public Handler<RoutingContext> create(Auth auth) {
@@ -26,21 +36,16 @@ public class AuthenticationFactory {
 			return null;
 		}
 		final AuthMethod authMethod = auth.method();
-		switch (authMethod) {
-			case BASIC:
-				return BasicAuthHandler.create(authProvider);
-			case JWT:
-				return JWTAuthHandler.create(authProvider);
-			case REDIRECT:
-				final String redirect = auth.redirectURL();
-				if ("".equals(redirect)) {
-					throw new IllegalArgumentException("You must specify a redirectURL if you're using Redirect Auth");
-				}
-				return RedirectAuthHandler.create(authProvider, redirect);
-			case API_TOKEN:
-				return new CheckTokenHandler(authProvider);
-			default:
-				throw new UnsupportedOperationException();
+		Function<AuthProvider, Handler<RoutingContext>> authHandlerCreator = authHandlers.get(authMethod);
+		if (authHandlerCreator == null && authMethod == REDIRECT) {
+			final String redirect = auth.redirectURL();
+			if ("".equals(redirect)) {
+				throw new IllegalArgumentException("You must specify a redirectURL if you're using Redirect Auth");
+			}
+			return RedirectAuthHandler.create(authProvider, redirect);
+		} else if (authHandlerCreator == null) {
+			throw new UnsupportedOperationException();
 		}
+		return authHandlerCreator.apply(authProvider);
 	}
 }
