c387efee110974244ade8b5d54868a53f54873e4
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-04-24 03:07:35 +0200

    wrap constant

--- src/main/java/com/github/aesteve/vertx/nubes/context/FileResolver.java
+++ src/main/java/com/github/aesteve/vertx/nubes/context/FileResolver.java
@@ -2,9 +2,13 @@ package com.github.aesteve.vertx.nubes.context;
 
 import io.vertx.ext.web.RoutingContext;
 
+import static com.github.aesteve.vertx.nubes.context.FileResolver.Constants.CONTEXT_FILE_NAME;
+
 public interface FileResolver {
 
-  String CONTEXT_FILE_NAME = "file-name";
+  final class Constants {
+    final static String CONTEXT_FILE_NAME = "file-name";
+  }
 
   static void resolve(RoutingContext context, String fileName) {
     context.put(CONTEXT_FILE_NAME, fileName);
