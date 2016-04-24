aa305d2bd72140eccc510a53115dd99c5183fee1
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-04-24 02:57:29 +0200

    utility class should be an interface

--- src/main/java/com/github/aesteve/vertx/nubes/context/FileResolver.java
+++ src/main/java/com/github/aesteve/vertx/nubes/context/FileResolver.java
@@ -2,15 +2,16 @@ package com.github.aesteve.vertx.nubes.context;
 
 import io.vertx.ext.web.RoutingContext;
 
-public class FileResolver {
+public interface FileResolver {
 
-  private static final String CONTEXT_FILE_NAME = "file-name";
+  String CONTEXT_FILE_NAME = "file-name";
 
-  public static void resolve(RoutingContext context, String fileName) {
+  static void resolve(RoutingContext context, String fileName) {
     context.put(CONTEXT_FILE_NAME, fileName);
   }
 
-  public static String getFileName(RoutingContext context) {
+  static String getFileName(RoutingContext context) {
     return context.get(CONTEXT_FILE_NAME);
   }
+
 }
