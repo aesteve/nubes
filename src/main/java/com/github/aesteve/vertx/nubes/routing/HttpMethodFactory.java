522a87e27e8a635a5452804ac865f1439d0e76ab
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-05-14 22:41:04 +0200

    complexity

--- src/main/java/com/github/aesteve/vertx/nubes/routing/HttpMethodFactory.java
+++ src/main/java/com/github/aesteve/vertx/nubes/routing/HttpMethodFactory.java
@@ -5,64 +5,77 @@ import io.vertx.core.http.HttpMethod;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
-public class HttpMethodFactory {
+public final class HttpMethodFactory {
+
+  private static List<Class<? extends Annotation>> annotClasses = Arrays.asList(
+      CONNECT.class,
+      DELETE.class,
+      GET.class,
+      HEAD.class,
+      OPTIONS.class,
+      PATCH.class,
+      POST.class,
+      PUT.class,
+      TRACE.class
+  );
+
+  private HttpMethodFactory() {}
 
   public static Map<HttpMethod, String> fromAnnotatedMethod(Method method) {
-    Map<HttpMethod, String> methods = new HashMap<>();
+    Map<HttpMethod, String> methods = new EnumMap<>(HttpMethod.class);
     for (Annotation annot : method.getDeclaredAnnotations()) {
       Class<? extends Annotation> annotClass = annot.annotationType();
-      if (annotClass.equals(CONNECT.class)) {
-        CONNECT connect = (CONNECT) annot;
-        methods.put(HttpMethod.CONNECT, connect.value());
-      }
-      if (annotClass.equals(DELETE.class)) {
-        DELETE delete = (DELETE) annot;
-        methods.put(HttpMethod.DELETE, delete.value());
-      }
-      if (annotClass.equals(GET.class)) {
-        GET get = (GET) annot;
-        methods.put(HttpMethod.GET, get.value());
-      }
-      if (annotClass.equals(HEAD.class)) {
-        HEAD head = (HEAD) annot;
-        methods.put(HttpMethod.HEAD, head.value());
-      }
-      if (annotClass.equals(OPTIONS.class)) {
-        OPTIONS options = (OPTIONS) annot;
-        methods.put(HttpMethod.OPTIONS, options.value());
-      }
-      if (annotClass.equals(PATCH.class)) {
-        PATCH patch = (PATCH) annot;
-        methods.put(HttpMethod.PATCH, patch.value());
-      }
-      if (annotClass.equals(POST.class)) {
-        POST post = (POST) annot;
-        methods.put(HttpMethod.POST, post.value());
-      }
-      if (annotClass.equals(PUT.class)) {
-        PUT put = (PUT) annot;
-        methods.put(HttpMethod.PUT, put.value());
-      }
-      if (annotClass.equals(TRACE.class)) {
-        TRACE trace = (TRACE) annot;
-        methods.put(HttpMethod.TRACE, trace.value());
-      }
+      putIfHttpMethod(methods, annot, annotClass);
     }
     return methods;
   }
 
   public static boolean isRouteMethod(Method method) {
-    return method.isAnnotationPresent(CONNECT.class)
-        || method.isAnnotationPresent(DELETE.class)
-        || method.isAnnotationPresent(GET.class)
-        || method.isAnnotationPresent(HEAD.class)
-        || method.isAnnotationPresent(OPTIONS.class)
-        || method.isAnnotationPresent(PATCH.class)
-        || method.isAnnotationPresent(POST.class)
-        || method.isAnnotationPresent(PUT.class)
-        || method.isAnnotationPresent(TRACE.class);
+    return annotClasses.stream().anyMatch(method::isAnnotationPresent);
   }
+
+  private static void putIfHttpMethod(Map<HttpMethod, String> methods, Annotation annot, Class<? extends Annotation> annotClass) {
+    if (annotClass.equals(CONNECT.class)) {
+      CONNECT connect = (CONNECT) annot;
+      methods.put(HttpMethod.CONNECT, connect.value());
+    }
+    if (annotClass.equals(DELETE.class)) {
+      DELETE delete = (DELETE) annot;
+      methods.put(HttpMethod.DELETE, delete.value());
+    }
+    if (annotClass.equals(GET.class)) {
+      GET get = (GET) annot;
+      methods.put(HttpMethod.GET, get.value());
+    }
+    if (annotClass.equals(HEAD.class)) {
+      HEAD head = (HEAD) annot;
+      methods.put(HttpMethod.HEAD, head.value());
+    }
+    if (annotClass.equals(OPTIONS.class)) {
+      OPTIONS options = (OPTIONS) annot;
+      methods.put(HttpMethod.OPTIONS, options.value());
+    }
+    if (annotClass.equals(PATCH.class)) {
+      PATCH patch = (PATCH) annot;
+      methods.put(HttpMethod.PATCH, patch.value());
+    }
+    if (annotClass.equals(POST.class)) {
+      POST post = (POST) annot;
+      methods.put(HttpMethod.POST, post.value());
+    }
+    if (annotClass.equals(PUT.class)) {
+      PUT put = (PUT) annot;
+      methods.put(HttpMethod.PUT, put.value());
+    }
+    if (annotClass.equals(TRACE.class)) {
+      TRACE trace = (TRACE) annot;
+      methods.put(HttpMethod.TRACE, trace.value());
+    }
+  }
+
 }
