50d0e3beadcedf95657795ca8251ff1fd67cc76a
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-05-14 18:33:23 +0200

    should be an interface

--- src/main/java/com/github/aesteve/vertx/nubes/context/ViewResolver.java
+++ src/main/java/com/github/aesteve/vertx/nubes/context/ViewResolver.java
@@ -2,15 +2,15 @@ package com.github.aesteve.vertx.nubes.context;
 
 import io.vertx.ext.web.RoutingContext;
 
-public class ViewResolver {
+public interface ViewResolver {
 
-  private static final String CONTEXT_TPL_NAME = "tpl-name";
+  String CONTEXT_TPL_NAME = "tpl-name";
 
-  public static void resolve(RoutingContext context, String viewName) {
+  static void resolve(RoutingContext context, String viewName) {
     context.put(CONTEXT_TPL_NAME, viewName);
   }
 
-  public static String getViewName(RoutingContext context) {
+  static String getViewName(RoutingContext context) {
     return context.get(CONTEXT_TPL_NAME);
   }
 }
