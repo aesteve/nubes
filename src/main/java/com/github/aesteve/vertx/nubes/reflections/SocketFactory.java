d606fcc41567bcc3bd1472ef3a08917a41c02b2d
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-05-14 22:18:29 +0200

    unused

--- src/main/java/com/github/aesteve/vertx/nubes/reflections/SocketFactory.java
+++ src/main/java/com/github/aesteve/vertx/nubes/reflections/SocketFactory.java
@@ -3,16 +3,12 @@ package com.github.aesteve.vertx.nubes.reflections;
 import com.github.aesteve.vertx.nubes.Config;
 import com.github.aesteve.vertx.nubes.annotations.sockjs.SockJS;
 import com.github.aesteve.vertx.nubes.reflections.visitors.SockJSVisitor;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 import org.reflections.Reflections;
 
 import java.util.Set;
 
 public class SocketFactory implements HandlerFactory {
-
-  private static final Logger LOG = LoggerFactory.getLogger(SocketFactory.class);
 
   private final Router router;
   private final Config config;
